9964dda81f8ac4ed78fe6f216d84a2ef09bb5571
Kirill Rodriguez <theoden8@users.noreply.github.com>
2017-07-14 15:03:43 +0100

    Added travis CI infrastructure (#22)

---
+++ .travis/computecpp_workaround.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VERSION=0.2.1
+
+ln -sf /tmp/ComputeCpp-CE-${VERSION}-Linux/ /tmp/ComputeCpp-latest
+
+cat .travis/additional_undef /tmp/ComputeCpp-latest/include/CL/sycl_builtins.h > /tmp/tmp_builtins
+mv /tmp/tmp_builtins /tmp/ComputeCpp-latest/include/CL/sycl_builtins.h.h
+
+cat .travis/additional_undef /tmp/ComputeCpp-latest/include/CL/host_relational_builtins.h > /tmp/tmp_builtins
+mv /tmp/tmp_builtins /tmp/ComputeCpp-latest/include/CL/host_relational_builtins.h
