96b35eb3c1d5ef45fba5142512d72743f4b70941
Mehdi Goli <mehdi.goli@codeplay.com>
2018-02-09 19:17:32 +0000

    Adding the correct directory for OpenBLAS.

--- .travis/build_OpenBLAS.sh
+++ .travis/build_OpenBLAS.sh
@@ -1,8 +1,10 @@
 ##########################
 # Get OpenBLAS
 ###########################
-wget https://github.com/xianyi/OpenBLAS/archive/develop.zip -O /tmp/OpenBLAS.zip
-unzip /tmp/OpenBLAS.zip -d /tmp/OpenBLAS  &> /dev/null
-cd /tmp/OpenBLAS
+cd /tmp
+git clone https://github.com/xianyi/OpenBLAS.git OpenBLAS
+cd OpenBLAS
 mkdir build
-make && make PREFIX=/tmp/OpenBLAS/build install
+cd build
+make -j8 -C ../
+make PREFIX=/tmp/OpenBLAS/build install
