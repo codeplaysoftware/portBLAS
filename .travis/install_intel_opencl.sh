4920e67aa7e297df92904d819256540c3cb0bbc8
Mehdi Goli <mehdi.goli@codeplay.com>
2018-02-09 12:22:49 +0000

    Updating the intel link

--- .travis/install_intel_opencl.sh
+++ .travis/install_intel_opencl.sh
@@ -6,7 +6,7 @@ set -ev
 # Get Intel OpenCL Runtime
 ###########################
 
-PACKAGE_URL=http://registrationcenter-download.intel.com/akdlm/irc_nas/12513/opencl_runtime_16.1.2_x64_rh_6.4.0.37.tgz
+PACKAGE_URL=http://registrationcenter-download.intel.com/akdlm/irc_nas/12556/opencl_runtime_16.1.2_x64_rh_6.4.0.37.tgz
 PACKAGE_NAME=opencl_runtime_16.1.2_x64_rh_6.4.0.37
 
 wget -q ${PACKAGE_URL} -O /tmp/opencl_runtime.tgz
