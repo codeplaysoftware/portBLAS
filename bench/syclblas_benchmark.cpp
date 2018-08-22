58f5a3e568b9d058546ffe71fe442e705edc9550
Adam Harries <adam.harries@codeplay.com>
2018-08-22 16:31:15 +0100

    limit sizes for GEMM, and add more variants

--- bench/syclblas_benchmark.cpp
+++ bench/syclblas_benchmark.cpp
@@ -380,9 +380,105 @@ class SyclBlasBenchmarker {
 
     return flops;
   }
+
+  BENCHMARK_FUNCTION(gemm_tn) {
+    using ScalarT = TypeParam;
+
+    size_t m_size = size * size;
+
+    auto lda = size;
+    auto ldb = size;
+    auto ldc = size;
+
+    auto m = size;
+    auto n = size;
+    auto k = size;
+
+    char const *t_a = "t";
+    char const *t_b = "n";
+
+    ScalarT alpha = ScalarT(1);
+    ScalarT beta = ScalarT(1);
+    // make two square matrices of size N * N
+    std::vector<ScalarT> a = random_data<ScalarT>(m_size);
+    std::vector<ScalarT> b = random_data<ScalarT>(m_size);
+    std::vector<ScalarT> c = const_data<ScalarT>(m_size, 0);
+
+    auto a_gpu = ex.template allocate<ScalarT>(m_size);
+    auto b_gpu = ex.template allocate<ScalarT>(m_size);
+    auto c_gpu = ex.template allocate<ScalarT>(m_size);
+
+    ex.copy_to_device(a.data(), a_gpu, m_size);
+    ex.copy_to_device(b.data(), b_gpu, m_size);
+    ex.copy_to_device(c.data(), c_gpu, m_size);
+
+    double flops = benchmark<>::measure(no_reps, m_size * 4, [&]() {
+      auto event = _gemm(ex, *t_a, *t_b, m, n, k, alpha, a_gpu, lda, b_gpu, ldb,
+                         beta, c_gpu, ldc);
+      ex.wait(event);
+    });
+
+    auto event = ex.copy_to_host(c_gpu, c.data(), m_size);
+
+    ex.wait(event);
+
+    ex.template deallocate<ScalarT>(a_gpu);
+    ex.template deallocate<ScalarT>(b_gpu);
+    ex.template deallocate<ScalarT>(c_gpu);
+
+    return flops;
+  }
+
+  BENCHMARK_FUNCTION(gemm_nt) {
+    using ScalarT = TypeParam;
+
+    size_t m_size = size * size;
+
+    auto lda = size;
+    auto ldb = size;
+    auto ldc = size;
+
+    auto m = size;
+    auto n = size;
+    auto k = size;
+
+    char const *t_a = "n";
+    char const *t_b = "t";
+
+    ScalarT alpha = ScalarT(1);
+    ScalarT beta = ScalarT(1);
+    // make two square matrices of size N * N
+    std::vector<ScalarT> a = random_data<ScalarT>(m_size);
+    std::vector<ScalarT> b = random_data<ScalarT>(m_size);
+    std::vector<ScalarT> c = const_data<ScalarT>(m_size, 0);
+
+    auto a_gpu = ex.template allocate<ScalarT>(m_size);
+    auto b_gpu = ex.template allocate<ScalarT>(m_size);
+    auto c_gpu = ex.template allocate<ScalarT>(m_size);
+
+    ex.copy_to_device(a.data(), a_gpu, m_size);
+    ex.copy_to_device(b.data(), b_gpu, m_size);
+    ex.copy_to_device(c.data(), c_gpu, m_size);
+
+    double flops = benchmark<>::measure(no_reps, m_size * 4, [&]() {
+      auto event = _gemm(ex, *t_a, *t_b, m, n, k, alpha, a_gpu, lda, b_gpu, ldb,
+                         beta, c_gpu, ldc);
+      ex.wait(event);
+    });
+
+    auto event = ex.copy_to_host(c_gpu, c.data(), m_size);
+
+    ex.wait(event);
+
+    ex.template deallocate<ScalarT>(a_gpu);
+    ex.template deallocate<ScalarT>(b_gpu);
+    ex.template deallocate<ScalarT>(c_gpu);
+
+    return flops;
+  }
 };
 
-BENCHMARK_MAIN_BEGIN(1 << 1, 1 << 24, 10);
+BENCHMARK_MAIN_BEGIN(1 << 1, 1 << 13, 10);
 SyclBlasBenchmarker<SYCL> blasbenchmark;
 
 BENCHMARK_REGISTER_FUNCTION("scal_float", scal_bench<float>);
@@ -396,6 +492,8 @@ BENCHMARK_REGISTER_FUNCTION("scal3op_float", scal3op_bench<float>);
 BENCHMARK_REGISTER_FUNCTION("axpy3op_float", axpy3op_bench<float>);
 BENCHMARK_REGISTER_FUNCTION("blas1_float", blas1_bench<float>);
 BENCHMARK_REGISTER_FUNCTION("gemm_nn_float", gemm_nn<float>);
+BENCHMARK_REGISTER_FUNCTION("gemm_tn_float", gemm_tn<float>);
+BENCHMARK_REGISTER_FUNCTION("gemm_nt_float", gemm_nt<float>);
 
 #ifndef NO_DOUBLE_SUPPORT
 BENCHMARK_REGISTER_FUNCTION("scal_double", scal_bench<double>);
@@ -409,5 +507,7 @@ BENCHMARK_REGISTER_FUNCTION("scal3op_double", scal3op_bench<double>);
 BENCHMARK_REGISTER_FUNCTION("axpy3op_double", axpy3op_bench<double>);
 BENCHMARK_REGISTER_FUNCTION("blas1_double", blas1_bench<double>);
 BENCHMARK_REGISTER_FUNCTION("gemm_nn_double", gemm_nn<double>);
+BENCHMARK_REGISTER_FUNCTION("gemm_tn_double", gemm_tn<double>);
+BENCHMARK_REGISTER_FUNCTION("gemm_nt_double", gemm_nt<double>);
 #endif
 BENCHMARK_MAIN_END();
