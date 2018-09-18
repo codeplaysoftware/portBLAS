90d5953e8b815656a985d09b0dea7b67916f0b18
Adam Harries <adam.harries@codeplay.com>
2018-09-18 16:01:20 +0100

    add level 2 benchmarks

--- bench/syclblas/syclblas_benchmark_level2.cpp
+++ bench/syclblas/syclblas_benchmark_level2.cpp
@@ -88,6 +88,60 @@ BENCHMARK(gemv, blas_level_2) {
   return flops;
 }
 
+
+BENCHMARK(ger, blas_level_2) {
+  using ScalarT = ElemT;
+
+  size_t m = std::get<0>(params);
+  size_t n = std::get<1>(params);
+
+
+  size_t n_fl_ops = m*n * 2;
+
+  size_t lda = m;
+  long incX = 1;
+  long incY = 1;
+  ScalarT prec = TestClass::template test_prec<test>();
+  ScalarT alpha = ScalarT(3);
+
+  // Input matrix
+  std::vector<ScalarT> a_v = benchmark<>::random_data<ScalarT>(m);
+  // Input Vector
+  std::vector<ScalarT> b_v(n);
+  // output Vector
+  std::vector<ScalarT> c_m_gpu_result(m * n, ScalarT(0));
+  // output system vector
+  std::vector<ScalarT> c_m_cpu(m * n, ScalarT(0));
+  TestClass::set_rand(a_v, m);
+  TestClass::set_rand(b_v, n);
+
+  // SYSTEM GEMMV
+  ger(m, n, alpha, a_v.data(), incX, b_v.data(), incY, c_m_cpu.data(), m);
+
+  SYCL_DEVICE_SELECTOR d;
+  auto q = TestClass::make_queue(d);
+  Executor<ExecutorType> ex(q);
+  auto v_a_gpu = ex.template allocate<ScalarT>(m);
+  auto v_b_gpu = ex.template allocate<ScalarT>(n);
+  auto m_c_gpu = ex.template allocate<ScalarT>(m * n);
+  ex.copy_to_device(a_v.data(), v_a_gpu, m);
+  ex.copy_to_device(b_v.data(), v_b_gpu, n);
+  ex.copy_to_device(c_m_gpu_result.data(), m_c_gpu, m * n);
+  // SYCLger
+  _ger(ex, m, n, alpha, v_a_gpu, incX, v_b_gpu, incY, m_c_gpu, m);
+
+  auto event = ex.copy_to_host(m_c_gpu, c_m_gpu_result.data(), m * n);
+  ex.wait(event);
+
+  for (size_t i = 0; i < m * n; ++i) {
+    ASSERT_NEAR(c_m_gpu_result[i], c_m_cpu[i], prec);
+  }
+  ex.template deallocate<ScalarT>(v_a_gpu);
+  ex.template deallocate<ScalarT>(v_b_gpu);
+  ex.template deallocate<ScalarT>(m_c_gpu);
+}
+
+
 SUITE(ADD(gemv))
 
 
