4fa6e576aab0c0e25c506afaee1720554d90f919
Adam Harries <adam.harries@codeplay.com>
2018-09-21 12:11:44 +0100

    run clang-format over level2 benchmarks

--- bench/syclblas/syclblas_benchmark_level2.cpp
+++ bench/syclblas/syclblas_benchmark_level2.cpp
@@ -43,10 +43,10 @@ BENCHMARK(gemv, blas_level_2) {
   size_t n = std::get<1>(params);
   const char* t_str = std::get<2>(params);
 
-  size_t vlen = t_str[0] == 'n' ? n : m; 
+  size_t vlen = t_str[0] == 'n' ? n : m;
   size_t rlen = t_str[0] == 'n' ? m : n;
 
-  size_t n_fl_ops = m*n * 2;
+  size_t n_fl_ops = m * n * 2;
 
   size_t lda = m;
   long incX = 1;
@@ -60,7 +60,8 @@ BENCHMARK(gemv, blas_level_2) {
   // Input Vector
   std::vector<ScalarT> b_v = benchmark<>::random_data<ScalarT>(vlen);
   // output Vector
-  std::vector<ScalarT> c_v_gpu_result = benchmark<>::const_data<ScalarT>(rlen, 0);
+  std::vector<ScalarT> c_v_gpu_result =
+      benchmark<>::const_data<ScalarT>(rlen, 0);
 
   auto m_a_gpu = ex.template allocate<ScalarT>(m * n);
   auto v_b_gpu = ex.template allocate<ScalarT>(vlen);
@@ -69,15 +70,14 @@ BENCHMARK(gemv, blas_level_2) {
   ex.copy_to_device(b_v.data(), v_b_gpu, vlen);
   ex.copy_to_device(c_v_gpu_result.data(), v_c_gpu, rlen);
 
-  benchmark<>::flops_units_t flops = benchmark<>::measure(
-      reps, n_fl_ops,
-      [&]() {
+  benchmark<>::flops_units_t flops =
+      benchmark<>::measure(reps, n_fl_ops, [&]() {
         auto event = _gemv(ex, *t_str, m, n, alpha, m_a_gpu, m, v_b_gpu, incX,
                            beta, v_c_gpu, incY);
         ex.wait(event);
       });
 
-      auto event = ex.copy_to_host(v_c_gpu, c_v_gpu_result.data(), rlen);
+  auto event = ex.copy_to_host(v_c_gpu, c_v_gpu_result.data(), rlen);
 
   ex.wait(event);
 
@@ -90,10 +90,9 @@ BENCHMARK(gemv, blas_level_2) {
 
 SUITE(ADD(gemv))
 
-
-// TODO: This doesn't seem to work with transposed matrices? 
+// TODO: This doesn't seem to work with transposed matrices?
 // Is it a problem with the routine, or the inputs?
 auto level_2_ranges = nd_range(size_range(2, 1024, 2), size_range(2, 1024, 2),
                                value_range({"n", "t"}));
 
-    BENCHMARK_MAIN(level_2_ranges, 10)
+BENCHMARK_MAIN(level_2_ranges, 10)
