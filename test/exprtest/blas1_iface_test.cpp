7c698e1b44cb4dc0cd95bfac487eaad825963f5e
mehdi-goli <goli.mehdi@gmail.com>
2018-10-10 16:38:21 +0100

    Adding reason for reducing the blas_iface_test size.

--- test/exprtest/blas1_iface_test.cpp
+++ test/exprtest/blas1_iface_test.cpp
@@ -28,7 +28,8 @@
 typedef ::testing::Types<blas_test_float<>, blas_test_double<>> BlasTypes;
 
 TYPED_TEST_CASE(BLAS_Test, BlasTypes);
-
+// The register size has been reduced in order to make sure that the output
+// result range is within the IEEE 754-1985 standard for floating-point numbers.
 REGISTER_SIZE(1023, interface1_test)
 REGISTER_STRD(1, interface1_test)
 REGISTER_PREC(float, 1e-4, interface1_test)
