0bb472b71450e1f4e8e1c3e21b93967e554cac60
Mehdi Goli <goli.mehdi@gmail.com>
2018-10-11 11:31:21 +0100

    Adding the cross compiling toolchain to sycl-blas. (#82)
    * Adding the cross compiling toolchain to sycl-blas.

--- test/exprtest/blas1_iface_test.cpp
+++ test/exprtest/blas1_iface_test.cpp
@@ -25,11 +25,12 @@
 
 #include "blas_test.hpp"
 
-typedef ::testing::Types<blas_test_args<double>> BlasTypes;
+typedef ::testing::Types<blas_test_float<>, blas_test_double<>> BlasTypes;
 
 TYPED_TEST_CASE(BLAS_Test, BlasTypes);
-
-REGISTER_SIZE(::RANDOM_SIZE, interface1_test)
+// The register size has been reduced in order to make sure that the output
+// result range is within the IEEE 754-1985 standard for floating-point numbers.
+REGISTER_SIZE(1023, interface1_test)
 REGISTER_STRD(1, interface1_test)
 REGISTER_PREC(float, 1e-4, interface1_test)
 REGISTER_PREC(double, 1e-6, interface1_test)
