2dec4c466edc0813a23318491e7aca0449c6e7e7
Adam Harries <adam.harries@codeplay.com>
2018-11-13 11:53:22 +0000

    move computecpp-sdk to modules/, update cmake, and set the include path in cmake

--- include/queue/queue_sycl.hpp
+++ include/queue/queue_sycl.hpp
@@ -26,11 +26,11 @@
 #define QUEUE_SYCL_HPP
 
 #include <CL/sycl.hpp>
-#include <computecpp-sdk/include/vptr/virtual_ptr.hpp>
 #include <queue/queue_base.hpp>
 #include <queue/sycl_iterator.hpp>
 #include <stdexcept>
 #include <types/sycl_types.hpp>
+#include <vptr/virtual_ptr.hpp>
 
 namespace blas {
 
