b43817a0e8eb1c1de972d8556c561ce0d3286b8b
Ralph Potter <ralph@codeplay.com>
2018-11-26 11:01:55 +0000

    Remove some event.waits() that only exist for debugging.

--- include/queue/queue_sycl.hpp
+++ include/queue/queue_sycl.hpp
@@ -228,7 +228,6 @@ class Queue_Interface<SYCL> {
           static_cast<generic_buffer_data_type *>(static_cast<void *>(const_cast<T*>(src))),
           write_acc);
     });
-    event.wait();
 
     return event;
   }
@@ -247,7 +246,6 @@ class Queue_Interface<SYCL> {
           blas::get_range_accessor<cl::sycl::access::mode::write>(dst, cgh);
       cgh.copy(src, acc);
     });
-    event.wait();
 
     return event;
   }
@@ -271,7 +269,6 @@ class Queue_Interface<SYCL> {
       cgh.copy(read_acc, static_cast<generic_buffer_data_type *>(
                              static_cast<void *>(dst)));
     });
-    event.wait();
 
     return event;
   }
@@ -289,7 +286,6 @@ class Queue_Interface<SYCL> {
           blas::get_range_accessor<cl::sycl::access::mode::read>(src, cgh);
       cgh.copy(acc, dst);
     });
-    event.wait();
 
     return event;
   }
