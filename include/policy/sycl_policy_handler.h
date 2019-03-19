15ab9f07e2b55aef5f962afb798fbfa322211dc8
mehdi-goli <goli.mehdi@gmail.com>
2019-03-19 18:33:10 +0000

    fixing sycl_policy_handler constructor for header only version.

--- include/policy/sycl_policy_handler.h
+++ include/policy/sycl_policy_handler.h
@@ -40,7 +40,18 @@ class PolicyHandler<codeplay_policy> {
  public:
   using policy_t = codeplay_policy;
 
-  explicit PolicyHandler(cl::sycl::queue q);
+  explicit PolicyHandler(cl::sycl::queue q)
+      : q_(q),
+        pointerMapperPtr_(std::shared_ptr<cl::sycl::codeplay::PointerMapper>(
+            new cl::sycl::codeplay::PointerMapper(),
+            [](cl::sycl::codeplay::PointerMapper *p) {
+              p->clear();
+              delete p;
+            })),
+        workGroupSize_(codeplay_policy::get_work_group_size(q)),
+        selectedDeviceType_(codeplay_policy::find_chosen_device_type(q)),
+        localMemorySupport_(codeplay_policy::has_local_memory(q)),
+        computeUnits_(codeplay_policy::get_num_compute_units(q)) {}
 
   template <typename element_t>
   element_t *allocate(size_t num_elements) const;
