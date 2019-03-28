ccc3c14f2d7e01e561daa8761e789503c76af27e
Adam Harries <harries.adam@gmail.com>
2019-03-28 17:20:43 +0000

    Remove commented opencl packages from install_intel_opencl.sh

--- .travis/install_intel_opencl.sh
+++ .travis/install_intel_opencl.sh
@@ -6,12 +6,6 @@ set -ev
 # Get Intel OpenCL Runtime
 ###########################
 
-# PACKAGE_URL=http://registrationcenter-download.intel.com/akdlm/irc_nas/12556/opencl_runtime_16.1.2_x64_rh_6.4.0.37.tgz
-# PACKAGE_NAME=opencl_runtime_16.1.2_x64_rh_6.4.0.37
-
-# PACKAGE_URL=http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/9019/opencl_runtime_16.1.1_x64_ubuntu_6.4.0.25.tgz
-# PACKAGE_NAME=opencl_runtime_16.1.1_x64_ubuntu_6.4.0.25
-
 PACKAGE_URL=http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/13793/l_opencl_p_18.1.0.013.tgz
 PACKAGE_NAME=l_opencl_p_18.1.0.013
 
