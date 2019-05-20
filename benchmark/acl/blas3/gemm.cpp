f98741493aeacf8d507207051f6720f9426594a3
Louis Sugy <louis.sugy@codeplay.com>
2019-05-20 12:59:04 +0100

    ACL benchmark integrated to googlebench

--- benchmark/acl/blas3/gemm.cpp
+++ benchmark/acl/blas3/gemm.cpp
@@ -1,1 +1,145 @@
-// totally doing nothing
+/***************************************************************************
+ *
+ *  @license
+ *  Copyright (C) 2016 Codeplay Software Limited
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  For your convenience, a copy of the License has been included in this
+ *  repository.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SYCL-BLAS: BLAS implementation using SYCL
+ *
+ *  @filename gemm.cpp
+ *
+ **************************************************************************/
+
+#include "utils.hpp"
+
+std::string get_name(std::string t1, std::string t2, int m, int k, int n) {
+  std::ostringstream str{};
+  str << "BM_Gemm<float>/" << t1 << "/" << t2 << "/" << m << "/" << k << "/"
+      << n;
+  return str.str();
+}
+
+void run(benchmark::State& state, int t1, int t2,
+         index_t m, index_t k, index_t n, float alpha, float beta) {
+  // Standard test setup.
+  std::string t1s = blas_benchmark::utils::from_transpose_enum(
+      static_cast<blas_benchmark::utils::Transposition>(t1));
+  std::string t2s = blas_benchmark::utils::from_transpose_enum(
+      static_cast<blas_benchmark::utils::Transposition>(t2));
+  const char* t_a = t1s.c_str();
+  const char* t_b = t2s.c_str();
+
+  index_t lda = t_a[0] == 'n' ? m : k;
+  index_t ldb = t_b[0] == 'n' ? k : n;
+  index_t ldc = m;
+
+  state.counters["m"] = m;
+  state.counters["k"] = k;
+  state.counters["n"] = n;
+
+  // The counters are double. We convert m, n and k to double to avoid
+  // integer overflows and write them in the counters
+  double m_d = static_cast<double>(m);
+  double n_d = static_cast<double>(n);
+  double k_d = static_cast<double>(k);
+
+  state.counters["n_fl_ops"] = 2 * (m_d * n_d * k_d) + 3 * (m_d * n_d);
+  state.counters["bytes_processed"] =
+      (m_d * k_d + k_d * n_d + 2 * m_d * n_d) * sizeof(float);
+  if (beta == 0.0) {
+    // not adding beta * C
+    state.counters["n_fl_ops"] -= 2 * m_d * n_d;
+    // not reading C
+    state.counters["bytes_processed"] -= m_d * n_d * sizeof(float);
+  }
+
+  // Matrices
+  std::vector<float> a = blas_benchmark::utils::random_data<float>(m * k);
+  std::vector<float> b = blas_benchmark::utils::random_data<float>(k * n);
+  std::vector<float> c =
+      blas_benchmark::utils::const_data<float>(m * n, 0);
+
+  // Device matrices
+  const arm_compute::TensorShape shape_a(k, m), shape_b(n, k), shape_c(n, m);
+  arm_compute::Tensor arm_a, arm_b, arm_c;
+  arm_a.allocator()->init(arm_compute::TensorInfo(shape_a, 1, arm_compute::DataType::F32));
+  arm_b.allocator()->init(arm_compute::TensorInfo(shape_b, 1, arm_compute::DataType::F32));
+  arm_c.allocator()->init(arm_compute::TensorInfo(shape_c, 1, arm_compute::DataType::F32));
+  arm_a.allocator()->allocate();
+  arm_b.allocator()->allocate();
+  arm_c.allocator()->allocate();
+  blas_benchmark::utils::fill_tensor(arm_a, a);
+  blas_benchmark::utils::fill_tensor(arm_b, b);
+  blas_benchmark::utils::fill_tensor(arm_c, c);
+
+  // Configure the BLAS routine
+  arm_compute::NEGEMM arm_gemm;
+  arm_gemm.configure(&arm_a, &arm_b, nullptr, &arm_c, alpha, beta);
+
+  // Create a utility lambda describing the blas method that we want to run.
+  auto blas_method_def = [&]() -> std::vector<void*> {
+    arm_gemm.run();
+    return {nullptr};
+  };
+
+  // Warm up to avoid benchmarking data transfer
+  // blas_benchmark::utils::warmup(blas_method_def);
+
+  blas_benchmark::utils::init_counters(state);
+
+  // Measure
+  for (auto _ : state) {
+    std::tuple<double, double> times =
+        blas_benchmark::utils::timef(blas_method_def);
+
+    // Report
+    blas_benchmark::utils::update_counters(state, times);
+  }
+
+  blas_benchmark::utils::calc_avg_counters(state);
+
+  arm_a.allocator()->free();
+  arm_b.allocator()->free();
+  arm_c.allocator()->free();
+};
+
+void register_benchmark(blas_benchmark::Args& args) {
+  auto gemm_params = blas_benchmark::utils::get_blas3_params<float>(args);
+
+  for (auto p : gemm_params) {
+    std::string t1s, t2s;
+    index_t m, n, k;
+    float alpha, beta;
+    std::tie(t1s, t2s, m, k, n, alpha, beta) = p;
+    int t1 = static_cast<int>(blas_benchmark::utils::to_transpose_enum(t1s));
+    int t2 = static_cast<int>(blas_benchmark::utils::to_transpose_enum(t2s));
+
+    auto BM_lambda = [&](benchmark::State& st, int t1,
+                         int t2, index_t m, index_t k, index_t n,
+                         float alpha, float beta) {
+      run(st, t1, t2, m, k, n, alpha, beta);
+    };
+    benchmark::RegisterBenchmark(get_name(t1s, t2s, m, k, n).c_str(),
+                                 BM_lambda, t1, t2, m, k, n, alpha,
+                                 beta);
+  }
+}
+
+namespace blas_benchmark {
+void create_benchmark(blas_benchmark::Args& args) {
+  register_benchmark(args);
+}
+}  // namespace blas_benchmark
---
+++ benchmark/acl/utils.hpp
@@ -0,0 +1,55 @@
+#ifndef ACL_UTILS_HPP
+#define ACL_UTILS_HPP
+
+#include <CL/cl.h>
+#include "utils/Utils.h"
+#include <arm_compute/core/Helpers.h>
+#include <arm_compute/core/ITensor.h>
+#include <arm_compute/core/Types.h>
+#include <arm_compute/core/Validate.h>
+#include <arm_compute/core/Window.h>
+#include <arm_compute/runtime/NEON/NEFunctions.h>
+#include <arm_compute/runtime/Tensor.h>
+#include <thread>
+
+#include "common_utils.hpp"
+
+namespace blas_benchmark {
+
+void create_benchmark(blas_benchmark::Args &args);
+
+namespace utils {
+
+template <typename tensor_t>
+void fill_tensor(tensor_t &tensor, std::vector<float>& src) {
+  arm_compute::Window window;
+  const arm_compute::TensorShape& shape = tensor.info()->tensor_shape();
+  window.use_tensor_dimensions(shape);
+
+  arm_compute::utils::map(tensor, true);
+
+  arm_compute::Iterator it(&tensor, window);
+
+  arm_compute::execute_window_loop(window,
+                      [&](const arm_compute::Coordinates &id) {
+                        int idx = id[0] * shape[1] + id[1];
+                        *reinterpret_cast<float *>(it.ptr()) = src[idx];
+                      },
+                      it);
+
+  arm_compute::utils::unmap(tensor);
+}
+
+/**
+ * @fn time_event
+ * @brief No event time for ACL. Return 0
+ */
+template <>
+inline cl_ulong time_event<void*>(void*& e) {
+  return 0;
+}
+
+}  // namespace utils
+}  // namespace blas_benchmark
+
+#endif
