2b76a366638002a3821a28ce6a5c0716a454ba1b
Morris Hafner <hafnermorris@gmail.com>
2019-05-22 23:47:54 +0100

    Change Templates to Jinja, remove old CI scripts

--- .travis/build_OpenBLAS.sh
+++
@@ -1,9 +0,0 @@
-##########################
-# Get OpenBLAS
-###########################
-cd /tmp
-git clone https://github.com/xianyi/OpenBLAS.git OpenBLAS
-cd OpenBLAS
-make 
-mkdir build
-make PREFIX=/tmp/OpenBLAS/build install
