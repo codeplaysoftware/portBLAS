7546a33607a7e4155119abd5915f94807306b2f6
Louis Sugy <louis.sugy@codeplay.com>
2019-08-14 15:21:10 +0100

    Optimize the AMD GPU Gemm parameterization backend (#176)
    * Optimized the AMD GPU Gemm parameterization backend

--- src/interface/blas3/backend/amd_gpu.hpp
+++ src/interface/blas3/backend/amd_gpu.hpp
@@ -37,9 +37,56 @@ typename executor_t::policy_t::event_t _gemm(
     executor_t& ex, index_t _M, index_t _N, index_t _K, element_t _alpha,
     container_0_t _a, index_t _lda, container_1_t _b, index_t _ldb,
     element_t _beta, container_2_t _c, index_t _ldc, index_t batch_size) {
-  if (_M == 10 && _N == 1024 && _K == 1024) {
+  /* Tall & Skinny matrices. */
+  if (batch_size == 1 && ((_K > 8192 && _M <= 1024 && _N <= 1024) ||
+                          (_K > 1024 && _M <= 256 && _N <= 256))) {
+    if (_M <= 16 && _N > 32) {
+      return blas::Gemm_Launcher<
+          256, true, true, true, 64, Tile<1, 4, 16, 16>, _t_a, _t_b,
+          static_cast<int>(gemm_memory_t::local),
+          static_cast<int>(gemm_algorithm_t::tall_skinny),
+          is_beta_zero>::template _select_gemm(ex, _M, _N, _K, _alpha, _a, _lda,
+                                               _b, _ldb, _beta, _c, _ldc, 1);
+    } else if (_M > 64 && _N <= 32) {
+      return blas::Gemm_Launcher<
+          256, true, true, true, 64, Tile<4, 1, 16, 16>, _t_a, _t_b,
+          static_cast<int>(gemm_memory_t::local),
+          static_cast<int>(gemm_algorithm_t::tall_skinny),
+          is_beta_zero>::template _select_gemm(ex, _M, _N, _K, _alpha, _a, _lda,
+                                               _b, _ldb, _beta, _c, _ldc, 1);
+    } else if (_M <= 16 || _N <= 16) {
+      return blas::Gemm_Launcher<
+          256, true, true, true, 64, Tile<1, 1, 16, 16>, _t_a, _t_b,
+          static_cast<int>(gemm_memory_t::local),
+          static_cast<int>(gemm_algorithm_t::tall_skinny),
+          is_beta_zero>::template _select_gemm(ex, _M, _N, _K, _alpha, _a, _lda,
+                                               _b, _ldb, _beta, _c, _ldc, 1);
+    } else if (_M <= 32 || _N <= 32) {
+      return blas::Gemm_Launcher<
+          256, true, true, true, 64, Tile<2, 2, 16, 16>, _t_a, _t_b,
+          static_cast<int>(gemm_memory_t::local),
+          static_cast<int>(gemm_algorithm_t::tall_skinny),
+          is_beta_zero>::template _select_gemm(ex, _M, _N, _K, _alpha, _a, _lda,
+                                               _b, _ldb, _beta, _c, _ldc, 1);
+    } else {
+      return blas::Gemm_Launcher<
+          256, true, true, true, 64, Tile<4, 4, 16, 16>, _t_a, _t_b,
+          static_cast<int>(gemm_memory_t::local),
+          static_cast<int>(gemm_algorithm_t::tall_skinny),
+          is_beta_zero>::template _select_gemm(ex, _M, _N, _K, _alpha, _a, _lda,
+                                               _b, _ldb, _beta, _c, _ldc, 1);
+    }
+  } else if (_M * _N <= 65536) {
     return blas::Gemm_Launcher<
-        256, true, false, false, 64, Tile<1, 1, 16, 16>, _t_a, _t_b,
+        256, false, false, false, 64, Tile<1, 1, 16, 16>, _t_a, _t_b,
+        static_cast<int>(gemm_memory_t::local),
+        static_cast<int>(gemm_algorithm_t::standard),
+        is_beta_zero>::template _select_gemm(ex, _M, _N, _K, _alpha, _a, _lda,
+                                             _b, _ldb, _beta, _c, _ldc,
+                                             batch_size);
+  } else if (_M * _N <= 262144) {
+    return blas::Gemm_Launcher<
+        256, false, false, false, 64, Tile<4, 1, 16, 16>, _t_a, _t_b,
         static_cast<int>(gemm_memory_t::local),
         static_cast<int>(gemm_algorithm_t::standard),
         is_beta_zero>::template _select_gemm(ex, _M, _N, _K, _alpha, _a, _lda,
