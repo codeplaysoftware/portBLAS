2b603808623704fd9179f6d70401e8c26fa79ffc
Ben Tracy <ben.tracy@codeplay.com>
2019-11-20 16:30:46 +0000

    Fix packetize aligned on certain platforms

--- src/operations/blas3/gemm_packetize.hpp
+++ src/operations/blas3/gemm_packetize.hpp
@@ -44,21 +44,23 @@ struct Packetize {
   template <bool internal, int ld, bool check_row = false,
             typename packet_t = cl::sycl::vec<value_t, packet_size>,
             typename SrcPointerType, typename DestPointerType, typename index_t,
-            typename EdgePredicate>
-  static SYCL_BLAS_INLINE typename std::enable_if<(internal == false)>::type
-  load(const bool in_range, SrcPointerType src, const index_t &src_offset,
-       DestPointerType dest, const index_t &dest_offset, EdgePredicate) {
+            typename EdgePredicate, bool align = aligned>
+  static SYCL_BLAS_INLINE
+      typename std::enable_if<(internal == false && !align)>::type
+      load(const bool in_range, SrcPointerType src, const index_t &src_offset,
+           DestPointerType dest, const index_t &dest_offset, EdgePredicate) {
     *(dest + dest_offset) = in_range ? *(src + src_offset) : value_t{0};
   }
 
   template <bool internal, int ld,
             typename packet_t = cl::sycl::vec<value_t, packet_size>,
             typename SrcPointerType, typename DestPointerType, typename index_t,
-            typename EdgePredicate>
-  static SYCL_BLAS_INLINE typename std::enable_if<(internal == true)>::type
-  load(const bool in_range, SrcPointerType src, const index_t &src_offset,
-       DestPointerType dest, const index_t &dest_offset,
-       EdgePredicate edge_in_range) {
+            typename EdgePredicate, bool align = aligned>
+  static SYCL_BLAS_INLINE
+      typename std::enable_if<(internal == true && !align)>::type
+      load(const bool in_range, SrcPointerType src, const index_t &src_offset,
+           DestPointerType dest, const index_t &dest_offset,
+           EdgePredicate edge_in_range) {
     packet_t packet{0};
     if (in_range) {
       using address_t = cl::sycl::access::address_space;
@@ -91,75 +93,75 @@ struct Packetize {
     using address_t = cl::sycl::access::address_space;
     packet.template store<address_t::local_space>(0, dest + dest_offset);
   }
-};
 
-// template <bool trans, size_t packet_size, typename value_t>
-// struct Packetize<true, trans, packet_size, value_t> {
-//   template <bool internal, int ld, bool check_row = false,
-//             typename packet_t = cl::sycl::vec<value_t, packet_size>,
-//             typename SrcPointerType, typename DestPointerType, typename
-//             index_t, typename EdgePredicate>
-//   static SYCL_BLAS_INLINE typename std::enable_if<(!internal)>::type load(
-//       const bool in_range, SrcPointerType src, const index_t &src_offset,
-//       DestPointerType dest, const index_t &dest_offset, EdgePredicate) {
-//     *(dest + dest_offset) = in_range ? *(src + src_offset) : value_t{0};
-//   }
+  // Aligned functions
+  template <bool internal, int ld, bool check_row = false,
+            typename packet_t = cl::sycl::vec<value_t, packet_size>,
+            typename SrcPointerType, typename DestPointerType, typename index_t,
+            typename EdgePredicate, bool align = aligned>
+  static SYCL_BLAS_INLINE typename std::enable_if<(!internal && align)>::type
+  load(const bool in_range, SrcPointerType src, const index_t &src_offset,
+       DestPointerType dest, const index_t &dest_offset, EdgePredicate) {
+    *(dest + dest_offset) = in_range ? *(src + src_offset) : value_t{0};
+  }
 
-//   template <bool internal, int ld, bool tran = trans,
-//             typename packet_t = cl::sycl::vec<value_t, packet_size>,
-//             typename SrcPointerType, typename DestPointerType, typename
-//             index_t, typename EdgePredicate>
-//   static SYCL_BLAS_INLINE typename std::enable_if<(internal && !tran)>::type
-//   load(const bool in_range, SrcPointerType src, const index_t &src_offset,
-//        DestPointerType dest, const index_t &dest_offset,
-//        EdgePredicate edge_in_range) {
-//     if (in_range) {
-//       *reinterpret_cast<packet_t *>(
-//           static_cast<value_t *>(dest + dest_offset)) =
-//           *reinterpret_cast<packet_t *>(
-//               static_cast<value_t *>(src + src_offset));
-//     } else {
-// #pragma unroll
-//       for (index_t i = 0; i < packet_size; i++) {
-//         *(dest + dest_offset) =
-//             edge_in_range(i) ? *(src + src_offset + i) : value_t{0};
-//       }
-//     }
-//   }
+  template <bool internal, int ld, bool tran = trans,
+            typename packet_t = cl::sycl::vec<value_t, packet_size>,
+            typename SrcPointerType, typename DestPointerType, typename index_t,
+            typename EdgePredicate, bool align = aligned>
+  static SYCL_BLAS_INLINE
+      typename std::enable_if<(internal && !tran && align)>::type
+      load(const bool in_range, SrcPointerType src, const index_t &src_offset,
+           DestPointerType dest, const index_t &dest_offset,
+           EdgePredicate edge_in_range) {
+    if (in_range) {
+      *reinterpret_cast<packet_t *>(
+          static_cast<value_t *>(dest + dest_offset)) =
+          *reinterpret_cast<packet_t *>(
+              static_cast<value_t *>(src + src_offset));
+    } else {
+#pragma unroll
+      for (index_t i = 0; i < packet_size; i++) {
+        *(dest + dest_offset) =
+            edge_in_range(i) ? *(src + src_offset + i) : value_t{0};
+      }
+    }
+  }
 
-//   template <bool internal, int ld, bool tran = trans,
-//             typename packet_t = cl::sycl::vec<value_t, packet_size>,
-//             typename SrcPointerType, typename DestPointerType, typename
-//             index_t, typename EdgePredicate>
-//   static SYCL_BLAS_INLINE typename std::enable_if<(internal && tran)>::type
-//   load(const bool in_range, SrcPointerType src, const index_t &src_offset,
-//        DestPointerType dest, const index_t &dest_offset,
-//        EdgePredicate edge_in_range) {
-//     packet_t packet{0};
-//     if (in_range) {
-//       *reinterpret_cast<packet_t *>(&packet) = *reinterpret_cast<packet_t *>(
-//           static_cast<value_t *>(src + src_offset));
-//     } else {
-// #pragma unroll
-//       for (index_t i = 0; i < packet_size; i++) {
-//         reinterpret_cast<value_t *>(&packet)[i] =
-//             edge_in_range(i) ? *(src + src_offset + i) : 0;
-//       }
-//     }
-//     store<ld>(packet, dest, dest_offset);
-//   }
+  template <bool internal, int ld, bool tran = trans,
+            typename packet_t = cl::sycl::vec<value_t, packet_size>,
+            typename SrcPointerType, typename DestPointerType, typename index_t,
+            typename EdgePredicate, bool align = aligned>
+  static SYCL_BLAS_INLINE
+      typename std::enable_if<(internal && tran && align)>::type
+      load(const bool in_range, SrcPointerType src, const index_t &src_offset,
+           DestPointerType dest, const index_t &dest_offset,
+           EdgePredicate edge_in_range) {
+    packet_t packet{0};
+    if (in_range) {
+      packet = *reinterpret_cast<packet_t *>(
+          static_cast<value_t *>(src + src_offset));
+      store<ld>(packet, dest, dest_offset);
+    } else {
+#pragma unroll
+      for (index_t i = 0; i < packet_size; i++) {
+        reinterpret_cast<value_t *>(&packet)[i] =
+            edge_in_range(i) ? *(src + src_offset + i) : 0;
+      }
+      store<ld>(packet, dest, dest_offset);
+    }
+  }
 
-//   template <int ld, typename packet_t = cl::sycl::vec<value_t, packet_size>,
-//             typename DestPointerType, typename index_t>
-//   static SYCL_BLAS_INLINE void store(packet_t &packet, DestPointerType dest,
-//                                      const index_t &dest_offset) {
-// #pragma unroll
-//     for (index_t i = 0; i < packet_size; i++) {
-//       *(dest + dest_offset + ld * i) = reinterpret_cast<value_t
-//       *>(&packet)[i];
-//     }
-//   }
-// };
+  template <int ld, typename packet_t = cl::sycl::vec<value_t, packet_size>,
+            typename DestPointerType, typename index_t, bool align = aligned>
+  static SYCL_BLAS_INLINE typename std::enable_if<align>::type store(
+      packet_t &packet, DestPointerType dest, const index_t &dest_offset) {
+#pragma unroll
+    for (index_t i = 0; i < packet_size; i++) {
+      *(dest + dest_offset + ld * i) = reinterpret_cast<value_t *>(&packet)[i];
+    }
+  }
+};  // namespace blas
 
 }  // namespace blas
 #endif  // SYCL_BLAS_BLAS3_GEMM_PACKETIZE_HPP
