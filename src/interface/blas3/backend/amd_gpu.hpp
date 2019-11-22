5645efacbe7106df7bc341e0d971bb844893afa1
Ben Tracy <ben.tracy@codeplay.com>
2019-11-22 17:35:24 +0000

    Updated AMD backend for GEMM/GEMV

--- src/interface/blas3/backend/amd_gpu.hpp
+++ src/interface/blas3/backend/amd_gpu.hpp
@@ -94,14 +94,12 @@ typename executor_t::policy_t::event_t _gemm(
         _t_a, _t_b, static_cast<int>(gemm_memory_t::local),
         static_cast<int>(gemm_algorithm_t::standard), is_beta_zero,
         2>::template _select_gemm(ex, _M, _N, _K, _alpha, _a, _lda, _b, _ldb,
-                                  _beta, _c, _ldc, batch_size);
   } else {
-    return blas::Gemm_Launcher<
-        256, false, false, false, ClSize, Tile<8, 8, tileWgSize, tileWgSize>,
-        _t_a, _t_b, static_cast<int>(gemm_memory_t::local),
+    256, false, false, false, 64, Tile<4, 4, 16, 16>, _t_a, _t_b,
+        static_cast<int>(gemm_memory_t::local),
         static_cast<int>(gemm_algorithm_t::standard), is_beta_zero,
-        2>::template _select_gemm(ex, _M, _N, _K, _alpha, _a, _lda, _b, _ldb,
-                                  _beta, _c, _ldc, batch_size);
+        2 > ::template _select_gemm(ex, _M, _N, _K, _alpha, _a, _lda, _b, _ldb,
+                                    _beta, _c, _ldc, batch_size);
   }
 }
 }  // namespace backend
