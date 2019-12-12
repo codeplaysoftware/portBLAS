3887f8189c4095eacab79f85bdbe1e7083d58b29
John Lawson <john@codeplay.com>
2019-12-12 11:05:54 +0000

    Generate kernels in separate TUs

---
+++ tools/auto_tuner/include/tuner_types.hpp
@@ -0,0 +1,110 @@
+/***************************************************************************
+ *
+ *  @license
+ *  Copyright (C) Codeplay Software Limited
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  For your convenience, a copy of the License has been included in this
+ *  repository.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SYCL-BLAS: BLAS implementation using SYCL
+ *
+ *  @filename tuner_types.hpp
+ *
+ **************************************************************************/
+
+#ifndef SYCLBLAS_TOOLS_AUTO_TUNER_TUNER_TYPES_HPP_
+#define SYCLBLAS_TOOLS_AUTO_TUNER_TUNER_TYPES_HPP_
+
+#include <iostream>
+#include <vector>
+
+#include "sycl_blas.hpp"
+
+using SYCLExecutor =
+    ::blas::Executor<::blas::PolicyHandler<::blas::codeplay_policy>>;
+
+template <typename DataType>
+using HostContainer = std::vector<DataType>;
+
+template <typename DataType>
+using DeviceContainer =
+    typename ::blas::BufferIterator<DataType, ::blas::codeplay_policy>;
+
+template <typename DataType>
+using MatrixContainer =
+    typename ::blas::MatrixViewTypeFactory<::blas::codeplay_policy, DataType,
+                                           int, ::blas::col_major>::output_t;
+
+struct TestResultEntry {
+  std::string name;
+  double sec;
+  double gflops;
+  double error;
+
+  TestResultEntry(std::string name) : name(name) {}
+
+  void print() const {
+    std::cout << gflops << " gflops: " << name << " - Time: " << sec
+              << " ms, Error: " << error << "\n";
+  }
+
+  bool operator<(const TestResultEntry &other) const {
+    return gflops < other.gflops;
+  }
+  bool operator>(const TestResultEntry &other) const {
+    return gflops > other.gflops;
+  }
+};
+
+class TestResult : public std::vector<TestResultEntry> {
+ public:
+  void print_all() const {
+    std::cout << "== Performance Results ==\n";
+    for (auto &r : *this) {
+      if (r.error < 0.1) {
+        r.print();
+      }
+    }
+  }
+};
+
+template <bool _TransA, bool _TransB, ::blas::gemm_memory_t _MemoryMode,
+          ::blas::gemm_algorithm_t _ShapeMode>
+struct GemmConfig {
+  static constexpr auto TransA = _TransA;
+  static constexpr auto TransB = _TransB;
+  static constexpr auto MemoryMode = _MemoryMode;
+  static constexpr auto ShapeMode = _ShapeMode;
+};
+
+template <typename element_t>
+struct GemmArgs {
+  int m;
+  int n;
+  int k;
+  element_t alpha;
+  const DeviceContainer<element_t> &a;
+  int lda;
+  const DeviceContainer<element_t> &b;
+  int ldb;
+  element_t beta;
+  const HostContainer<element_t> &init_c;
+  DeviceContainer<element_t> &c;
+  HostContainer<element_t> &output_c;
+  int ldc;
+  int batch_size;
+  const HostContainer<element_t> &expected_c;
+};
+
+#endif  // SYCLBLAS_TOOLS_AUTO_TUNER_TUNER_TYPES_HPP_
