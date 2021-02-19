8ea6458c3fa3cf9e2b3c9a1ef0f660fdcc69a0c9
Ben Tracy <ben.tracy@codeplay.com>
2021-02-19 11:19:57 +0000

    test change

--- src/interface/gemm_launcher.hpp
+++ src/interface/gemm_launcher.hpp
@@ -19,7 +19,7 @@
  *
  *  SYCL-BLAS: BLAS implementation using SYCL
  *
- *  @filename gemm_launcher.hpp
+ *  @filename gemm_launcher.hp
  *
  **************************************************************************/
 
