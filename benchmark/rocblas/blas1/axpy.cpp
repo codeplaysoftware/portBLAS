9934bada542819d80249f38241fae6f6aa7c2833
Ouadie EL FAROUKI <ouadie.elfarouki@codeplay.com>
2023-03-07 12:44:32 +0000

    Fixed copyright headers (rocblas benchmarks)

--- benchmark/rocblas/blas1/axpy.cpp
+++ benchmark/rocblas/blas1/axpy.cpp
@@ -17,6 +17,8 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
+ *  SYCL-BLAS: BLAS implementation using SYCL
+ * 
  *  @filename axpy.cpp
  *
  **************************************************************************/
--- benchmark/rocblas/blas1/rotmg.cpp
+++ benchmark/rocblas/blas1/rotmg.cpp
@@ -1,4 +1,4 @@
-/**************************************************************************
+/***************************************************************************
  *
  *  @license
  *  Copyright (C) Codeplay Software Limited
@@ -16,6 +16,8 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
+ *
+ *  SYCL-BLAS: BLAS implementation using SYCL
  *
  *  @filename rotmg.cpp
  *
