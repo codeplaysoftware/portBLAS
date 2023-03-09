43ca70289db9aba9414c4bd98bc679809299dbc9
Muhammad Tanvir <muhammad.tanvir@codeplay.com>
2023-03-09 17:39:56 +0000

    Cleaned up benchmarks by adding license and renaming params object

--- benchmark/syclblas/blas3/gemm_batched.cpp
+++ benchmark/syclblas/blas3/gemm_batched.cpp
@@ -1,7 +1,7 @@
 /**************************************************************************
  *
  *  @license
- *  Copyright (C) 2016 Codeplay Software Limited
+ *  Copyright (C) Codeplay Software Limited
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
