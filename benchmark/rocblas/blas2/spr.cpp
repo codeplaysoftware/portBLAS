1062bcf5329088fe8cc84ef0ff1d3925a26b3f0e
Ouadie EL FAROUKI <ouadie.elfarouki@codeplay.com>
2023-03-14 10:19:49 +0000

    Added spr benchmark

---
+++ benchmark/rocblas/blas2/spr.cpp
@@ -0,0 +1,184 @@
+/**************************************************************************
+ *
+ *  @license
+ *  Copyright (C) Codeplay Software Limited
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  For your convenience, a copy of the License has been included in this
+ *  repository.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SYCL-BLAS: BLAS implementation using SYCL
+ *
+ *  @filename spr.cpp
+ *
+ **************************************************************************/
+
+#include "../utils.hpp"
+
+template <typename scalar_t>
+std::string get_name(char uplo, int n, scalar_t alpha, int incX) {
+  std::ostringstream str{};
+  str << "BM_Spr<" << blas_benchmark::utils::get_type_name<scalar_t>() << ">/"
+      << uplo << "/" << n << "/" << alpha << "/" << incX;
+  return str.str();
+}
+
+template <typename scalar_t, typename... args_t>
+static inline void rocblas_spr_f(args_t&&... args) {
+  if constexpr (std::is_same_v<scalar_t, float>) {
+    CHECK_ROCBLAS_STATUS(rocblas_sspr(std::forward<args_t>(args)...));
+  } else if constexpr (std::is_same_v<scalar_t, double>) {
+    CHECK_ROCBLAS_STATUS(rocblas_dspr(std::forward<args_t>(args)...));
+  }
+  return;
+}
+
+template <typename scalar_t>
+void run(benchmark::State& state, rocblas_handle& rb_handle, char uplo, int n,
+         scalar_t alpha, int incX, bool* success) {
+  // The counters are double. We convert n to double to avoid
+  // integer overflows for n_fl_ops and bytes_processed
+  double size_d = static_cast<double>(n * (n + 1) / 2);
+
+  state.counters["size_d"] = size_d;
+  state.counters["alpha"] = static_cast<double>(alpha);
+  state.counters["incX"] = incX;
+
+  {
+    double nflops_XtimesX = 2.0 * size_d;
+    state.counters["n_fl_ops"] = n + nflops_XtimesX;
+  }
+  {
+    double mem_readA = size_d;
+    double mem_readX = static_cast<double>(n);
+    state.counters["bytes_processed"] =
+        (mem_readA + mem_readX) * sizeof(scalar_t);
+  }
+
+  // Matrix options (rocBLAS)
+  const rocblas_fill uplo_rb =
+      uplo == 'u' ? rocblas_fill_upper : rocblas_fill_lower;
+
+  // Data sizes
+  const int m_size = n * n;
+  const int v_size = 1 + (n - 1) * std::abs(incX);
+
+  // Input matrix/vector, output vector.
+  std::vector<scalar_t> m_a =
+      blas_benchmark::utils::random_data<scalar_t>(m_size);
+  std::vector<scalar_t> v_x =
+      blas_benchmark::utils::random_data<scalar_t>(v_size);
+
+  {
+    // Device memory allocation & H2D copy
+    blas_benchmark::utils::HIPVector<scalar_t> m_a_gpu(m_size, m_a.data());
+    blas_benchmark::utils::HIPVector<scalar_t> v_x_gpu(v_size, v_x.data());
+
+#ifdef BLAS_VERIFY_BENCHMARK
+    // Reference spr
+    std::vector<scalar_t> x_ref = v_x;
+    std::vector<scalar_t> m_a_ref = m_a;
+    reference_blas::spr<scalar_t>(&uplo, n, alpha, x_ref.data(), incX,
+                                  m_a_ref.data());
+
+    // Rocblas verification spr
+    std::vector<scalar_t> m_a_temp = m_a;
+    {
+      // Temp result on device (copied back to Host upon destruction)
+      blas_benchmark::utils::HIPVector<scalar_t, true> m_a_temp_gpu(
+          m_size, m_a_temp.data());
+      // rocBLAS function call
+      rocblas_spr_f<scalar_t>(rb_handle, uplo_rb, n, &alpha, v_x_gpu, incX,
+                              m_a_temp_gpu);
+    }
+
+    std::ostringstream err_stream;
+    if (!utils::compare_vectors(m_a_temp, m_a_ref, err_stream, "")) {
+      const std::string& err_str = err_stream.str();
+      state.SkipWithError(err_str.c_str());
+      *success = false;
+    };
+#endif
+
+    auto blas_warmup = [&]() -> void {
+      rocblas_spr_f<scalar_t>(rb_handle, uplo_rb, n, &alpha, v_x_gpu, incX,
+                              m_a_gpu);
+      CHECK_HIP_ERROR(hipStreamSynchronize(NULL));
+      return;
+    };
+
+    hipEvent_t start, stop;
+    CHECK_HIP_ERROR(hipEventCreate(&start));
+    CHECK_HIP_ERROR(hipEventCreate(&stop));
+
+    auto blas_method_def = [&]() -> std::vector<hipEvent_t> {
+      CHECK_HIP_ERROR(hipEventRecord(start, NULL));
+      rocblas_spr_f<scalar_t>(rb_handle, uplo_rb, n, &alpha, v_x_gpu, incX,
+                              m_a_gpu);
+      CHECK_HIP_ERROR(hipEventRecord(stop, NULL));
+      CHECK_HIP_ERROR(hipEventSynchronize(stop));
+      return std::vector{start, stop};
+    };
+
+    // Warmup
+    blas_benchmark::utils::warmup(blas_warmup);
+
+    blas_benchmark::utils::init_counters(state);
+
+    // Measure
+    for (auto _ : state) {
+      // Run
+      std::tuple<double, double> times =
+          blas_benchmark::utils::timef_hip(blas_method_def);
+
+      // Report
+      blas_benchmark::utils::update_counters(state, times);
+    }
+
+    blas_benchmark::utils::calc_avg_counters(state);
+
+    CHECK_HIP_ERROR(hipEventDestroy(start));
+    CHECK_HIP_ERROR(hipEventDestroy(stop));
+  }  // release device memory via utils::DeviceVector destructors
+}
+
+template <typename scalar_t>
+void register_benchmark(blas_benchmark::Args& args, rocblas_handle& rb_handle,
+                        bool* success) {
+  auto spr_params = blas_benchmark::utils::get_spr_params<scalar_t>(args);
+
+  for (auto p : spr_params) {
+    int n, incX;
+    std::string uplo;
+    scalar_t alpha;
+    std::tie(uplo, n, alpha, incX) = p;
+
+    char uplo_str = uplo[0];
+
+    auto BM_lambda_col = [&](benchmark::State& st, rocblas_handle rb_handle,
+                             char uplo, int n, scalar_t alpha, int incX,
+                             bool* success) {
+      run<scalar_t>(st, rb_handle, uplo, n, alpha, incX, success);
+    };
+    benchmark::RegisterBenchmark(
+        get_name<scalar_t>(uplo_str, n, alpha, incX).c_str(), BM_lambda_col,
+        rb_handle, uplo_str, n, alpha, incX, success);
+  }
+}
+
+namespace blas_benchmark {
+void create_benchmark(blas_benchmark::Args& args, rocblas_handle& rb_handle,
+                      bool* success) {
+  BLAS_REGISTER_BENCHMARK(args, rb_handle, success);
+}
+}  // namespace blas_benchmark
