7c825513504c0c46ecf5d0c21658fd5ac5d9eeb4
s-Nick <nicolo.scipione@codeplay.com>
2023-03-20 11:28:11 +0100

    [benchmark] Add gemm and trsm operators cuBLAS benchmark
    Signed-off-by: s-Nick <nicolo.scipione@codeplay.com>

---
+++ benchmark/cublas/blas3/trsm.cpp
@@ -0,0 +1,205 @@
+/**************************************************************************
+ *
+ *  @license
+ *  Copyright (C) Codeplay Software Limited
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  For your convenience, a copy of the License has been included in this
+ *  repository.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SYCL-BLAS: BLAS implementation using SYCL
+ *
+ *  @filename trsm.cpp
+ *
+ **************************************************************************/
+
+#include "../utils.hpp"
+
+template <typename scalar_t>
+std::string get_name(char side, char uplo, char trans, char diag, index_t m,
+                     index_t n) {
+  std::ostringstream str{};
+  str << "BM_Trsm<" << blas_benchmark::utils::get_type_name<scalar_t>() << ">/"
+      << side << "/" << uplo << "/" << trans << "/" << diag << "/" << m << "/"
+      << n;
+  return str.str();
+}
+
+template <typename scalar_t, typename... args_t>
+static inline void cublas_routine(args_t&&... args) {
+  if constexpr (std::is_same_v<scalar_t, float>) {
+    CUBLAS_CHECK(cublasStrsm(std::forward<args_t>(args)...));
+  } else if constexpr (std::is_same_v<scalar_t, double>) {
+    CUBLAS_CHECK(cublasDtrsm(std::forward<args_t>(args)...));
+  }
+  return;
+}
+
+template <typename scalar_t>
+void run(benchmark::State& state, cublasHandle_t* cuda_handle_ptr, char side,
+         char uplo, char trans, char diag, index_t m, index_t n, scalar_t alpha,
+         bool* success) {
+  // Standard test setup.
+  index_t lda = side == 'l' ? m : n;
+  index_t ldb = m;
+  index_t k = side == 'l' ? m : n;
+
+  cublasHandle_t& cuda_handle = *cuda_handle_ptr;
+
+  const int sizeA = k * lda;
+  const int sizeB = n * ldb;
+
+  // Init counters for benchmark
+
+  // The counters are double. We convert m, n and k to double to avoid
+  // integer overflows for n_fl_ops and bytes_processed
+  const double m_d = static_cast<double>(m);
+  const double n_d = static_cast<double>(n);
+  const double k_d = static_cast<double>(k);
+
+  state.counters["m"] = m_d;
+  state.counters["k"] = k_d;
+  state.counters["n"] = n_d;
+
+  const double mem_read = k_d * (k_d + 1) / 2;
+  const double mem_write = m_d * n_d;
+
+  const double total_mem = (mem_read * mem_write) * sizeof(scalar_t);
+  state.counters["bytes_processed"] = total_mem;
+
+  const double nflops_AtimesB =
+      2 * k_d * (k_d + 1) / 2 * (side == 'l' ? n_d : m_d);
+  const double nflops_timesAlpha = m_d * n_d;
+  const double nflops = nflops_AtimesB + nflops_timesAlpha;
+  state.counters["n_fl_ops"] = nflops;
+
+  // Matrices
+  std::vector<scalar_t> a(sizeA);
+  std::vector<scalar_t> b = blas_benchmark::utils::random_data<scalar_t>(sizeB);
+
+  const scalar_t diagValue =
+      diag == 'u' ? scalar_t{1}
+                  : blas_benchmark::utils::random_scalar<scalar_t>(
+                        scalar_t{1}, scalar_t{10});
+
+  blas_benchmark::utils::fill_trsm_matrix(a, k, lda, uplo, diagValue,
+                                          scalar_t{0});
+
+  blas_benchmark::utils::CUDAVector<scalar_t> a_gpu(sizeA, a.data());
+  blas_benchmark::utils::CUDAVector<scalar_t> b_gpu(sizeB, b.data());
+
+  cublasSideMode_t c_side =
+      (side == 'l') ? CUBLAS_SIDE_LEFT : CUBLAS_SIDE_RIGHT;
+
+  cublasFillMode_t c_uplo =
+      (uplo == 'u') ? CUBLAS_FILL_MODE_UPPER : CUBLAS_FILL_MODE_LOWER;
+
+  cublasOperation_t c_t = (trans == 'n') ? CUBLAS_OP_N : CUBLAS_OP_T;
+
+  cublasDiagType_t c_diag =
+      (diag == 'u') ? CUBLAS_DIAG_UNIT : CUBLAS_DIAG_NON_UNIT;
+
+#ifdef BLAS_VERIFY_BENCHMARK
+  // Run once verifying the results against the reference blas implementation.
+  std::vector<scalar_t> x_ref = b;
+  std::vector<scalar_t> b_temp = b;
+
+  reference_blas::trsm(&side, &uplo, &trans, &diag, m, n,
+                       static_cast<scalar_t>(alpha), a.data(), lda,
+                       x_ref.data(), ldb);
+
+  {
+    blas_benchmark::utils::CUDAVector<scalar_t, true> b_temp_gpu(sizeB,
+                                                                 b_temp.data());
+    cublas_routine<scalar_t>(cuda_handle, c_side, c_uplo, c_t, c_diag, m, n,
+                             &alpha, a_gpu, lda, b_temp_gpu, ldb);
+  }
+
+  std::ostringstream err_stream;
+  if (!utils::compare_vectors(b_temp, x_ref, err_stream, "")) {
+    const std::string& err_str = err_stream.str();
+    state.SkipWithError(err_str.c_str());
+    *success = false;
+  };
+#endif
+  auto blas_warmup = [&]() -> void {
+    cublas_routine<scalar_t>(cuda_handle, c_side, c_uplo, c_t, c_diag, m, n,
+                             &alpha, a_gpu, lda, b_gpu, ldb);
+    return;
+  };
+
+  cudaEvent_t start, stop;
+  CUDA_CHECK(cudaEventCreate(&start));
+  CUDA_CHECK(cudaEventCreate(&stop));
+
+  auto blas_method_def = [&]() -> std::vector<cudaEvent_t> {
+    CUDA_CHECK(cudaEventRecord(start, NULL));
+    cublas_routine<scalar_t>(cuda_handle, c_side, c_uplo, c_t, c_diag, m, n,
+                             &alpha, a_gpu, lda, b_gpu, ldb);
+    CUDA_CHECK(cudaEventRecord(stop, NULL));
+    CUDA_CHECK(cudaEventSynchronize(stop));
+    return std::vector{start, stop};
+  };
+
+  // Warmup
+  blas_benchmark::utils::warmup(blas_warmup);
+  CUDA_CHECK(cudaStreamSynchronize(NULL));
+
+  blas_benchmark::utils::init_counters(state);
+
+  // Measure
+  for (auto _ : state) {
+    // Run
+    std::tuple<double, double> times =
+        blas_benchmark::utils::timef_cuda(blas_method_def);
+
+    // Report
+    blas_benchmark::utils::update_counters(state, times);
+  }
+
+  state.SetBytesProcessed(state.iterations() * total_mem);
+  state.SetItemsProcessed(state.iterations() * nflops);
+
+  blas_benchmark::utils::calc_avg_counters(state);
+};
+
+template <typename scalar_t>
+void register_benchmark(blas_benchmark::Args& args,
+                        cublasHandle_t* cuda_handle_ptr, bool* success) {
+  auto trsm_params = blas_benchmark::utils::get_trsm_params<scalar_t>(args);
+
+  for (auto p : trsm_params) {
+    char side, uplo, trans, diag;
+    index_t m, n;
+    scalar_t alpha;
+    std::tie(side, uplo, trans, diag, m, n, alpha) = p;
+
+    auto BM_lambda = [&](benchmark::State& st, cublasHandle_t* cuda_handle_ptr,
+                         char side, char uplo, char trans, char diag, index_t m,
+                         index_t n, scalar_t alpha, bool* success) {
+      run<scalar_t>(st, cuda_handle_ptr, side, uplo, trans, diag, m, n, alpha,
+                    success);
+    };
+    benchmark::RegisterBenchmark(
+        get_name<scalar_t>(side, uplo, trans, diag, m, n).c_str(), BM_lambda,
+        cuda_handle_ptr, side, uplo, trans, diag, m, n, alpha, success)
+        ->UseRealTime();
+  }
+}
+
+namespace blas_benchmark {
+void create_benchmark(blas_benchmark::Args& args,
+                      cublasHandle_t* cuda_handle_ptr, bool* success) {
+  BLAS_REGISTER_BENCHMARK(args, cuda_handle_ptr, success);
+}
+}  // namespace blas_benchmark
