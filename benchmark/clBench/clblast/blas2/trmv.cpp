97c87f011fb0c6fb3bc2882eab2c3f13f83ec46c
s-Nick <nicolo.scipione@codeplay.com>
2023-04-03 16:25:05 +0200

    [benchmark] Add trmv operator to CLBLAST benchmarks

---
+++ benchmark/clBench/clblast/blas2/trmv.cpp
@@ -0,0 +1,189 @@
+/**************************************************************************
+ *
+ *  @license
+ *  Copyright (C) Codeplay Software Limited
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  For your convenience, a copy of the License has been included in this
+ *  repository.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SYCL-BLAS: BLAS implementation using SYCL
+ *
+ *  @filename trmv.cpp
+ *
+ **************************************************************************/
+
+#include "../utils.hpp"
+
+template <typename scalar_t>
+std::string get_name(std::string uplo, std::string t, std::string diag, int n) {
+  std::ostringstream str{};
+  str << "BM_Trmv<" << blas_benchmark::utils::get_type_name<scalar_t>() << ">/"
+      << uplo << "/" << t << "/" << diag << "/" << n;
+  return str.str();
+}
+
+template <typename scalar_t>
+void run(benchmark::State& state, ExecutorType* executorPtr, std::string uplo,
+         std::string t, std::string diag, index_t n, bool* success) {
+  // Standard test setup.
+  const char* uplo_str = uplo.c_str();
+  const char* t_str = t.c_str();
+  const char* diag_str = diag.c_str();
+
+  index_t xlen = n;
+  index_t lda = n;
+  index_t incX = 1;
+
+  // The counters are double. We convert n to double to avoid
+  // integer overflows for n_fl_ops and bytes_processed
+  const double n_d = static_cast<double>(n);
+
+  state.counters["n"] = n_d;
+
+  // Compute the number of A non-zero elements.
+  const double A_validVal = n_d * (n_d + 1) / 2;
+
+  const double nflops_tot = 2 * A_validVal;
+  state.counters["n_fl_ops"] = nflops_tot;
+
+  const double mem_readA = A_validVal;
+  const double mem_readX = n_d;
+  const double mem_writeX = n_d;
+  const double tot_mem_processed =
+      (mem_readA + mem_readX + mem_writeX) * sizeof(scalar_t);
+  state.counters["bytes_processed"] = tot_mem_processed;
+
+  ExecutorType& sb_handle = *executorPtr;
+
+  // Input matrix/vector, output vector.
+  std::vector<scalar_t> m_a(lda * n);
+  std::vector<scalar_t> v_x =
+      blas_benchmark::utils::random_data<scalar_t>(xlen);
+
+  // Populate the correct triangular matrix with larger values
+  // in column-major way
+  for (int i = 0; i < n; ++i)
+    for (int j = 0; j < n; ++j)
+      m_a[(j * lda) + i] =
+          (*uplo_str == 'u') ? ((i >= j) ? blas_benchmark::utils::random_scalar(
+                                               scalar_t{9}, scalar_t{11})
+                                         : 0)
+                             : ((i <= j) ? blas_benchmark::utils::random_scalar(
+                                               scalar_t{9}, scalar_t{11})
+                                         : 0);
+
+  // Specify the triangle.
+  clblast::Triangle a_uplo =
+      blas_benchmark::utils::translate_triangle(uplo_str);
+
+  // Specify the transposition.
+  clblast::Transpose a_tr =
+      blas_benchmark::utils::translate_transposition(t_str);
+
+  // Specify the unit-diagonal.
+  clblast::Diagonal a_diag =
+      blas_benchmark::utils::translate_diagonal(diag_str);
+
+  // Specify the layout.
+  auto layout = clblast::Layout::kColMajor;
+
+  MemBuffer<scalar_t> m_a_gpu(executorPtr, m_a.data(),
+                              static_cast<size_t>(lda * n));
+  MemBuffer<scalar_t> v_x_gpu(executorPtr, v_x.data(),
+                              static_cast<size_t>(xlen));
+
+#ifdef BLAS_VERIFY_BENCHMARK
+  // Run a first time with a verification of the results
+  std::vector<scalar_t> v_x_ref = v_x;
+  reference_blas::trmv(uplo_str, t_str, diag_str, n, m_a.data(), lda,
+                       v_x_ref.data(), incX);
+  std::vector<scalar_t> v_x_temp = v_x;
+  {
+    MemBuffer<scalar_t> v_x_temp_gpu(executorPtr, v_x_temp.data(),
+                                     static_cast<size_t>(xlen));
+    cl_event event;
+    clblast::Trmv<scalar_t>(layout, a_uplo, a_tr, a_diag, n, m_a_gpu.dev(), 0,
+                            lda, v_x_temp_gpu.dev(), 0, incX,
+                            executorPtr->_queue(), &event);
+    CLEventHandler::wait(event);
+  }
+
+  std::ostringstream err_stream;
+  if (!utils::compare_vectors(v_x_temp, v_x_ref, err_stream, "")) {
+    const std::string& err_str = err_stream.str();
+    state.SkipWithError(err_str.c_str());
+    *success = false;
+  };
+#endif
+
+  auto blas_method_def = [&]() -> std::vector<cl_event> {
+    cl_event event;
+    clblast::Trmv<scalar_t>(layout, a_uplo, a_tr, a_diag, n, m_a_gpu.dev(), 0,
+                            lda, v_x_gpu.dev(), 0, incX, executorPtr->_queue(),
+                            &event);
+    CLEventHandler::wait(event);
+    return std::vector{event};
+  };
+
+  // Warmup
+  blas_benchmark::utils::warmup(blas_method_def);
+
+  blas_benchmark::utils::init_counters(state);
+
+  // Measure
+  for (auto _ : state) {
+    // Run
+    std::tuple<double, double> times =
+        blas_benchmark::utils::timef(blas_method_def);
+
+    // Report
+    blas_benchmark::utils::update_counters(state, times);
+  }
+
+  state.SetBytesProcessed(state.iterations() * tot_mem_processed);
+  state.SetItemsProcessed(state.iterations() * nflops_tot);
+
+  blas_benchmark::utils::calc_avg_counters(state);
+}
+
+template <typename scalar_t>
+void register_benchmark(blas_benchmark::Args& args, ExecutorType* executorPtr,
+                        bool* success) {
+  // trmv uses the same parameters as trsv
+  auto trmv_params = blas_benchmark::utils::get_trsv_params(args);
+
+  for (auto p : trmv_params) {
+    std::string uplos;
+    std::string ts;
+    std::string diags;
+    index_t n;
+    std::tie(uplos, ts, diags, n) = p;
+
+    auto BM_lambda = [&](benchmark::State& st, ExecutorType* executorPtr,
+                         std::string uplos, std::string ts, std::string diags,
+                         index_t n, bool* success) {
+      run<scalar_t>(st, executorPtr, uplos, ts, diags, n, success);
+    };
+    benchmark::RegisterBenchmark(
+        get_name<scalar_t>(uplos, ts, diags, n).c_str(), BM_lambda, executorPtr,
+        uplos, ts, diags, n, success);
+  }
+}
+
+namespace blas_benchmark {
+void create_benchmark(blas_benchmark::Args& args, ExecutorType* executorPtr,
+                      bool* success) {
+  BLAS_REGISTER_BENCHMARK(args, executorPtr, success);
+}
+}  // namespace blas_benchmark
