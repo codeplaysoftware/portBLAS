6a9b09194e94ad793561faf9e3b2c2fd50a2a63b
Nicolò Scipione <nicolo.scipione@codeplay.com>
2023-04-06 15:02:21 +0200

    Add missing benchmarks for level 2 operators (#393)
    This PR adds benchmarks for those operators available in SYCL-BLAS. 
    It also adds the clblast counterpart.

---
+++ benchmark/clBench/clblast/blas2/ger.cpp
@@ -0,0 +1,166 @@
+/**************************************************************************
+ *
+ *  @license
+ *  Copyright (C) Codeplay Software Limited
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  For your convenience, a copy of the License has been included in this
+ *  repository.
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  SYCL-BLAS: BLAS implementation using SYCL
+ *
+ *  @filename ger.cpp
+ *
+ **************************************************************************/
+
+#include "../utils.hpp"
+
+template <typename scalar_t>
+std::string get_name(int m, int n) {
+  std::ostringstream str{};
+  str << "BM_Ger<" << blas_benchmark::utils::get_type_name<scalar_t>() << ">/"
+      << m << "/" << n;
+  return str.str();
+}
+
+template <typename scalar_t>
+void run(benchmark::State& state, ExecutorType* executorPtr, index_t m,
+         index_t n, scalar_t alpha, bool* success) {
+  // Standard test setup.
+
+  index_t xlen = m;
+  index_t ylen = n;
+
+  index_t lda = m;
+  index_t incX = 1;
+  index_t incY = 1;
+
+  // The counters are double. We convert m and n to double to avoid
+  // integer overflows for n_fl_ops and bytes_processed
+  const double m_d = static_cast<double>(m);
+  const double n_d = static_cast<double>(n);
+
+  state.counters["m"] = m_d;
+  state.counters["n"] = n_d;
+
+  const double nflops_timesAlpha = std::min(m, n);
+  const double nflops_XtimesYplusA = 2 * m_d * n_d;
+  const double nflops_tot = nflops_XtimesYplusA + nflops_timesAlpha;
+  state.counters["n_fl_ops"] = nflops_tot;
+
+  const double mem_readWriteA = 2 * m_d * n_d;
+  const double mem_readX = xlen;
+  const double mem_readY = ylen;
+  const double tot_mem_processed =
+      (mem_readWriteA + mem_readX + mem_readY) * sizeof(scalar_t);
+  state.counters["bytes_processed"] = tot_mem_processed;
+
+  ExecutorType& sb_handle = *executorPtr;
+
+  // Input matrix/vector, output vector.
+  std::vector<scalar_t> m_a =
+      blas_benchmark::utils::random_data<scalar_t>(m * n);
+  std::vector<scalar_t> v_x =
+      blas_benchmark::utils::random_data<scalar_t>(xlen);
+  std::vector<scalar_t> v_y =
+      blas_benchmark::utils::random_data<scalar_t>(ylen);
+
+  auto layout = clblast::Layout::kColMajor;
+
+  MemBuffer<scalar_t> m_a_gpu(executorPtr, m_a.data(),
+                              static_cast<size_t>(m * n));
+  MemBuffer<scalar_t> v_x_gpu(executorPtr, v_x.data(),
+                              static_cast<size_t>(xlen));
+  MemBuffer<scalar_t> v_y_gpu(executorPtr, v_y.data(),
+                              static_cast<size_t>(ylen));
+
+#ifdef BLAS_VERIFY_BENCHMARK
+  // Run a first time with a verification of the results
+  std::vector<scalar_t> m_a_ref = m_a;
+  reference_blas::ger(m, n, alpha, v_x.data(), incX, v_y.data(), incY,
+                      m_a_ref.data(), lda);
+  std::vector<scalar_t> m_a_temp(m_a);
+
+  {
+    MemBuffer<scalar_t> m_a_temp_gpu(executorPtr, m_a_temp.data(),
+                                     static_cast<size_t>(m * n));
+    cl_event event;
+    clblast::Ger(layout, m, n, alpha, v_x_gpu.dev(), 0, incX, v_y_gpu.dev(), 0,
+                 incY, m_a_temp_gpu.dev(), 0, lda, executorPtr->_queue(),
+                 &event);
+    CLEventHandler::wait(event);
+  }
+
+  std::ostringstream err_stream;
+  if (!utils::compare_vectors(m_a_temp, m_a_ref, err_stream, "")) {
+    const std::string& err_str = err_stream.str();
+    state.SkipWithError(err_str.c_str());
+    *success = false;
+  };
+
+#endif
+
+  auto blas_method_def = [&]() -> std::vector<cl_event> {
+    cl_event event;
+    clblast::Ger(layout, m, n, alpha, v_x_gpu.dev(), 0, incX, v_y_gpu.dev(), 0,
+                 incY, m_a_gpu.dev(), 0, lda, executorPtr->_queue(), &event);
+    CLEventHandler::wait(event);
+    return {event};
+  };
+
+  // Warmup
+  blas_benchmark::utils::warmup(blas_method_def);
+
+  blas_benchmark::utils::init_counters(state);
+
+  // Measure
+  for (auto _ : state) {
+    // Run
+    std::tuple<double, double> times =
+        blas_benchmark::utils::timef(blas_method_def);
+
+    // Report
+    blas_benchmark::utils::update_counters(state, times);
+  }
+
+  state.SetBytesProcessed(state.iterations() * tot_mem_processed);
+  state.SetItemsProcessed(state.iterations() * nflops_tot);
+
+  blas_benchmark::utils::calc_avg_counters(state);
+}
+
+template <typename scalar_t>
+void register_benchmark(blas_benchmark::Args& args, ExecutorType* executorPtr,
+                        bool* success) {
+  auto ger_params = blas_benchmark::utils::get_ger_params<scalar_t>(args);
+
+  for (auto p : ger_params) {
+    index_t m, n;
+    scalar_t alpha;
+    std::tie(m, n, alpha) = p;
+
+    auto BM_lambda = [&](benchmark::State& st, ExecutorType* executorPtr,
+                         index_t m, index_t n, scalar_t alpha, bool* success) {
+      run<scalar_t>(st, executorPtr, m, n, alpha, success);
+    };
+    benchmark::RegisterBenchmark(get_name<scalar_t>(m, n).c_str(), BM_lambda,
+                                 executorPtr, m, n, alpha, success);
+  }
+}
+
+namespace blas_benchmark {
+void create_benchmark(blas_benchmark::Args& args, ExecutorType* executorPtr,
+                      bool* success) {
+  BLAS_REGISTER_BENCHMARK(args, executorPtr, success);
+}
+}  // namespace blas_benchmark
