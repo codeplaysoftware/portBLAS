170f9a5d13e76ea4ba6c5b3a66e47787c7bafe31
aacostadiaz <127198532+aacostadiaz@users.noreply.github.com>
2023-04-14 09:59:51 +0100

    Update symm.cpp
    Fix counters in symm benchmark

--- benchmark/syclblas/blas3/symm.cpp
+++ benchmark/syclblas/blas3/symm.cpp
@@ -104,22 +104,30 @@ void run(benchmark::State& state, blas::SB_Handle* sb_handle_ptr, char side,
   {
     // The counters are double. We convert m, n and k to double to avoid
     // integer overflows for n_fl_ops and bytes_processed
+        // The counters are double. We convert m, n and k to double to avoid
+    // integer overflows for n_fl_ops and bytes_processed
     double m_d = static_cast<double>(m);
     double n_d = static_cast<double>(n);
-    double k_d = static_cast<double>(k);
 
     state.counters["m"] = m_d;
-    state.counters["k"] = k_d;
     state.counters["n"] = n_d;
 
-    double mem_read = (k_d * (k_d + 1) / 2) + 2 * (m_d * n_d);
-    double mem_write = m_d * n_d;
-    double total_mem = (mem_read + mem_write) * sizeof(scalar_t);
+    const double mem_readBreadC =
+        (beta != scalar_t{0}) ? 2 * m_d * n_d : m_d * n_d;
+    const double mem_writeC = m_d * n_d;
+    const double mem_readA =
+        (side == 'l') ? (m_d * (m_d + 1) / 2) : (n_d * (n_d + 1) / 2);
+    const double total_mem =
+        (mem_readBreadC + mem_writeC + mem_readA) * sizeof(scalar_t);
     state.counters["bytes_processed"] = total_mem;
-    state.SetBytesProcessed(state.iterations() * total_mem);
 
-    double nflops = 2 * (k_d * k_d * n_d) + 2 * (m_d * n_d);
+    const double nflops_AtimesB =
+        (side == 'l') ? (2 * m_d * m_d) * n_d : 2 * n_d * n_d * n_d;
+    const double nflops_addBetaC = 2 * m_d * n_d;
+    const double nflops = nflops_AtimesB + nflops_addBetaC;
     state.counters["n_fl_ops"] = nflops;
+
+    state.SetBytesProcessed(state.iterations() * total_mem);
     state.SetItemsProcessed(state.iterations() * nflops);
   }
 
