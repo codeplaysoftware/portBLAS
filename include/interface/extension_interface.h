c94523fce3ef1f0bb470a2b0deaef91dd8070d2d
s-Nick <nicolo.scipione@codeplay.com>
2023-05-08 17:36:12 +0200

    Add omatadd operator basic implementation and test
    Signed-off-by: s-Nick <nicolo.scipione@codeplay.com>

--- include/interface/extension_interface.h
+++ include/interface/extension_interface.h
@@ -44,6 +44,15 @@ typename sb_handle_t::event_t _matcopy(sb_handle_t& sb_handle, char trans,
                                        index_t in_stride, out_t out_memory,
                                        index_t ld_out, index_t out_stride);
 
+template <typename sb_handle_t, typename element_t, typename index_t,
+          typename container_t>
+typename sb_handle_t::event_t _omatadd(sb_handle_t& sb_handle, char trans_a,
+                                       char trans_b, index_t m, index_t n,
+                                       element_t alpha, container_t a,
+                                       index_t lda, element_t beta,
+                                       container_t b, index_t ldb,
+                                       container_t c, index_t ldc);
+
 template <typename operator_t, typename element_t, typename sb_handle_t,
           typename input_t, typename output_t, typename index_t>
 typename sb_handle_t::event_t _reduction(sb_handle_t& sb_handle,
@@ -85,6 +94,18 @@ typename sb_handle_t::event_t _omatcopy2(sb_handle_t& sb_handle, char trans,
                                          index_t ld_out, index_t out_stride) {
   return internal::_matcopy(sb_handle, trans, m, n, alpha, in_memory, ld_in,
                             in_stride, out_memory, ld_out, out_stride);
+}
+
+template <typename sb_handle_t, typename element_t, typename index_t,
+          typename container_t>
+typename sb_handle_t::event_t _omatadd(sb_handle_t& sb_handle, char trans_a,
+                                       char trans_b, index_t m, index_t n,
+                                       element_t alpha, container_t a,
+                                       index_t lda, element_t beta,
+                                       container_t b, index_t ldb,
+                                       container_t c, index_t ldc) {
+  return internal::_omatadd(sb_handle, trans_a, trans_b, m, n, alpha, a, lda,
+                            beta, b, ldb, c, ldc);
 }
 
 template <typename operator_t, typename element_t, typename sb_handle_t,
--- src/interface/extension_interface.hpp
+++ src/interface/extension_interface.hpp
@@ -27,6 +27,8 @@
 #define SYCL_BLAS_EXTENSION_INTERFACE_HPP
 
 #include "blas_meta.h"
+#include "operations/blas1_trees.h"
+#include "operations/blas_operators.hpp"
 #include "operations/extension/matcopy.h"
 #include "operations/extension/reduction.h"
 #include "sb_handle/sycl_blas_handle.h"
@@ -94,6 +96,46 @@ _matcopy_impl(sb_handle_t& sb_handle, index_t m, index_t n, element_t alpha,
     auto copy_op = make_op<Assign>(out_view, scal_op);
     ret = sb_handle.execute(copy_op);
   }
+  return ret;
+}
+
+template <bool trans_a, bool trans_b, typename sb_handle_t, typename element_t,
+          typename index_t, typename container_t>
+typename std::enable_if<trans_a || trans_b, typename sb_handle_t::event_t>::type
+_omatadd_impl(sb_handle_t& sb_handle, index_t m, index_t n, element_t alpha,
+              container_t a, index_t lda, element_t beta, container_t b,
+              index_t ldb, container_t c, index_t ldc) {
+  typename sb_handle_t::event_t ret;
+  /*
+  auto m_a_view = make_matrix_view<col_major>(a, m, n, lda);
+  auto m_b_view = make_matrix_view<col_major>(b, m, n, ldb);
+  auto m_c_view = make_matrix_view<col_major>(c, m, n, ldc);
+  if constexpr (trans_a){}
+  if constexpr (trans_b){}
+  auto scal_a = make_op<ScalarOp, ProductOperator>(alpha, m_a_view);
+  auto scal_b = make_op<ScalarOp, ProductOperator>(beta, m_b_view);
+  auto sum_matrix = make_op<BinaryOp, AddOperator>(scal_a, scal_b);
+  auto copy_op = make_op<Assign>(m_c_view, sum_matrix);
+  return sb_handle.execute(copy_op);
+  */
+  return ret;
+}
+template <bool trans_a, bool trans_b, typename sb_handle_t, typename element_t,
+          typename index_t, typename container_t>
+typename std::enable_if<!trans_a && !trans_b,
+                        typename sb_handle_t::event_t>::type
+_omatadd_impl(sb_handle_t& sb_handle, index_t m, index_t n, element_t alpha,
+              container_t a, index_t lda, element_t beta, container_t b,
+              index_t ldb, container_t c, index_t ldc) {
+  typename sb_handle_t::event_t ret;
+  auto m_a_view = make_matrix_view<col_major>(a, m, n, lda);
+  auto m_b_view = make_matrix_view<col_major>(b, m, n, ldb);
+  auto m_c_view = make_matrix_view<col_major>(c, m, n, ldc);
+  auto scal_a = make_op<ScalarOp, ProductOperator>(alpha, m_a_view);
+  auto scal_b = make_op<ScalarOp, ProductOperator>(beta, m_b_view);
+  auto sum_op = make_op<BinaryOp, AddOperator>(scal_a, scal_b);
+  auto copy_op = make_op<Assign>(m_c_view, sum_op);
+  ret = sb_handle.execute(copy_op);
   return ret;
 }
 
@@ -193,6 +235,23 @@ typename sb_handle_t::event_t _matcopy(sb_handle_t& sb_handle, char trans,
     return _matcopy_impl<false>(sb_handle, m, n, alpha, in_memory, ld_in,
                                 in_stride, out_memory, ld_out, out_stride);
   }
+}
+
+template <typename sb_handle_t, typename element_t, typename index_t,
+          typename container_t>
+typename sb_handle_t::event_t _omatadd(sb_handle_t& sb_handle, char trans_a,
+                                       char trans_b, index_t m, index_t n,
+                                       element_t alpha, container_t a,
+                                       index_t lda, element_t beta,
+                                       container_t b, index_t ldb,
+                                       container_t c, index_t ldc) {
+  // typename sb_handle_t::event_t ret;
+  if (trans_a == 't' || trans_b == 't') {
+    typename sb_handle_t::event_t ret;
+    return ret;
+  }
+  return _omatadd_impl<false, false>(sb_handle, m, n, alpha, a, lda, beta, b,
+                                     ldb, c, ldc);
 }
 
 template <typename operator_t, typename element_t, typename sb_handle_t,
