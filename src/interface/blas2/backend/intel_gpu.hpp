3c7b689fd3a4354044f7eee687e7de19c1bc5b8a
pgorlani <paolo.gorlani@codeplay.com>
2023-06-30 19:03:59 +0100

    Adjust Intel_GPU parameters

--- src/interface/blas2/backend/intel_gpu.hpp
+++ src/interface/blas2/backend/intel_gpu.hpp
@@ -135,8 +135,8 @@ template <uplo_type uplo, transpose_type trn, diag_type diag,
 typename sb_handle_t::event_t _trsv(sb_handle_t& sb_handle, index_t _N,
                                     container_t0 _mA, index_t _lda,
                                     container_t1 _vx, increment_t _incx) {
-  return blas::internal::_trsv_impl<16, 4, uplo, trn, diag>(sb_handle, _N, _mA,
-                                                            _lda, _vx, _incx);
+  return blas::internal::_trsv_impl<8, 4, uplo, trn, diag>(sb_handle, _N, _mA,
+                                                           _lda, _vx, _incx);
 }
 }  // namespace backend
 }  // namespace trsv
@@ -149,7 +149,7 @@ template <uplo_type uplo, transpose_type trn, diag_type diag,
 typename sb_handle_t::event_t _tbsv(sb_handle_t& sb_handle, index_t _N,
                                     index_t _K, container_t0 _mA, index_t _lda,
                                     container_t1 _vx, increment_t _incx) {
-  return blas::internal::_tbsv_impl<16, 4, uplo, trn, diag>(
+  return blas::internal::_tbsv_impl<8, 4, uplo, trn, diag>(
       sb_handle, _N, _K, _mA, _lda, _vx, _incx);
 }
 }  // namespace backend
@@ -163,8 +163,8 @@ template <uplo_type uplo, transpose_type trn, diag_type diag,
 typename sb_handle_t::event_t _tpsv(sb_handle_t& sb_handle, index_t _N,
                                     container_t0 _mA, container_t1 _vx,
                                     increment_t _incx) {
-  return blas::internal::_tpsv_impl<16, 4, uplo, trn, diag>(sb_handle, _N, _mA,
-                                                            _vx, _incx);
+  return blas::internal::_tpsv_impl<8, 4, uplo, trn, diag>(sb_handle, _N, _mA,
+                                                           _vx, _incx);
 }
 }  // namespace backend
 }  // namespace tpsv
