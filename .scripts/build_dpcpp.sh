26b8c4be9634d3d194a751e2e17bf0e25ac78d88
aacostadiaz <127198532+aacostadiaz@users.noreply.github.com>
2023-08-03 15:12:04 +0100

    Update dpcpp version in the Github CI (#452)
    This pull request updates the dpcpp version used in Github CI to
    2023-07-27 nightly.

--- .scripts/build_dpcpp.sh
+++ .scripts/build_dpcpp.sh
@@ -5,7 +5,7 @@ set -ev
 ###########################
 # Get DPCPP
 ###########################
-wget --no-verbose https://github.com/intel/llvm/releases/download/sycl-nightly/20221201/dpcpp-compiler.tar.gz -O dpcpp-compiler.tar.gz
+wget --no-verbose https://github.com/intel/llvm/releases/download/sycl-nightly/20230727/dpcpp-compiler.tar.gz -O dpcpp-compiler.tar.gz
 rm -rf /tmp/dpcpp && mkdir /tmp/dpcpp/
 tar -xzf dpcpp-compiler.tar.gz -C /tmp/dpcpp --strip-components 1
 ls -R /tmp/dpcpp/
