7d9e8879251f9384cc1ebe08c59c39d3cb4d3755
nscipione <nicolo.scipione@codeplay.com>
2024-05-02 09:36:34 +0100

    Apply changes also to AMD GPUs
    The fixes provided for iamax, iamin, tbsv, tpsv and trsv for NVIDIA
    target architectural feature in common between the two GPUs. So
    implementing the same changes also for AMD GPUs target fix failing
    tests.
    
    Signed-off-by: nscipione <nicolo.scipione@codeplay.com>

--- src/interface/blas2/backend/default.hpp
+++ src/interface/blas2/backend/default.hpp
@@ -137,7 +137,7 @@ typename sb_handle_t::event_t _trsv(
     sb_handle_t& sb_handle, index_t _N, container_t0 _mA, index_t _lda,
     container_t1 _vx, increment_t _incx,
     typename sb_handle_t::event_t _dependencies) {
-#ifdef NVIDIA
+#if defined(NVIDIA) || defined (AMD)
   return blas::internal::_trsv_impl<32, 4, uplo, trn, diag>(
       sb_handle, _N, _mA, _lda, _vx, _incx, _dependencies);
 #else
@@ -157,7 +157,7 @@ typename sb_handle_t::event_t _tbsv(
     sb_handle_t& sb_handle, index_t _N, index_t _K, container_t0 _mA,
     index_t _lda, container_t1 _vx, increment_t _incx,
     const typename sb_handle_t::event_t& _dependencies) {
-#ifdef NVIDIA
+#if defined(NVIDIA) || defined (AMD)
   return blas::internal::_tbsv_impl<32, 4, uplo, trn, diag>(
       sb_handle, _N, _K, _mA, _lda, _vx, _incx, _dependencies);
 #else
@@ -176,7 +176,7 @@ template <uplo_type uplo, transpose_type trn, diag_type diag,
 typename sb_handle_t::event_t _tpsv(
     sb_handle_t& sb_handle, index_t _N, container_t0 _mA, container_t1 _vx,
     increment_t _incx, const typename sb_handle_t::event_t& _dependencies) {
-#ifdef NVIDIA
+#if defined(NVIDIA) || defined (AMD)
   return blas::internal::_tpsv_impl<32, 4, uplo, trn, diag>(
       sb_handle, _N, _mA, _vx, _incx, _dependencies);
 #else
