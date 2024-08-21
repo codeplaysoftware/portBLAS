c128d670119e99598bf257afc5ebe9caedce28ae
nscipione <nicolo.scipione@codeplay.com>
2024-08-21 13:37:16 +0100

    Change how free temporary memory
    host_task caused issues with some backends, so changing to a
    synchronization and a simple sycl::free when using usm.
    Removing synchronization on copy since it is not necessary anymore.

--- src/interface/blas1_interface.hpp
+++ src/interface/blas1_interface.hpp
@@ -826,21 +826,15 @@ typename sb_handle_t::event_t _rotmg(
     auto y1_view = make_vector_view(_y1_tmp, inc, vector_size);
     auto operation = Rotmg<decltype(d1_view)>(d1_view, d2_view, x1_view,
                                               y1_view, param_view);
-#if !defined(AMD_GPU) && !defined(NVIDIA_GPU)
-    // This memcpy requires a wait to enforce synchronization for usm. This is
-    // due to workaround a bug present in OpenCL backend that shows up with
-    // portBLAS implementation. Otherwise event dependencies works fine. The
-    // issue has been reported to intel/llvm project here:
-    // https://github.com/intel/llvm/issues/14623
-    // Same issue arises on Intel ARC and Intel iGPU, not sure if Intel PVC
-    // hides the problem or it is not present. For extra safety introducing this
-    // sync.
-    if constexpr (mem_type != helper::AllocType::buffer) copy_y1.wait();
-#endif
+
     auto operator_event =
         sb_handle.execute(operation, typename sb_handle_t::event_t{copy_y1});
-    blas::helper::enqueue_deallocate(operator_event, _y1_tmp,
-                                     sb_handle.get_queue());
+    if constexpr (mem_type != helper::AllocType::buffer) {
+      // This wait is necessary to free the temporary memory created above and
+      // avoiding the host_task
+      operator_event[0].wait();
+      sycl::free(_y1_tmp, sb_handle.get_queue());
+    }
     return operator_event;
   } else {
     auto y1_view = make_vector_view(_y1, inc, vector_size);
